2d9cdf87eb2008c25eebedb6db6a42cf9465e36f
Kendall Morgan <1841529+kendallm@users.noreply.github.com>
2022-12-02 11:04:34 -0800

    Remove types for map

--- src/aoc_2022/day2.rs
+++ src/aoc_2022/day2.rs
@@ -4,19 +4,19 @@ use std::collections::HashMap;
 pub fn run() {
     let input = get_inputs(2022, 2);
     
-    let mut scores = HashMap::<String, (i32, i32)>::new();
+    let mut scores = HashMap::new();
 
-    scores.insert(String::from("A X"), (4, 3));
-    scores.insert("A Y".into(), (8, 4));
-    scores.insert("A Z".into(), (3, 8));
+    scores.insert("A X".to_string(), (4, 3));
+    scores.insert("A Y".to_string(), (8, 4));
+    scores.insert("A Z".to_string(), (3, 8));
     
-    scores.insert(String::from("B X"), (1, 1));
-    scores.insert("B Y".into(), (5, 5));
-    scores.insert("B Z".into(), (9, 9));
+    scores.insert("B X".to_string(), (1, 1));
+    scores.insert("B Y".to_string(), (5, 5));
+    scores.insert("B Z".to_string(), (9, 9));
 
-    scores.insert(String::from("C X"), (7, 2));
-    scores.insert("C Y".into(), (2, 6));
-    scores.insert("C Z".into(), (6, 7));
+    scores.insert("C X".to_string(), (7, 2));
+    scores.insert("C Y".to_string(), (2, 6));
+    scores.insert("C Z".to_string(), (6, 7));
 
     let mut part1 = 0;
     let mut part2 = 0;
